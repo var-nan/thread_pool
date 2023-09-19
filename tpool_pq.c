7de7e2dd342c15bd2c20916593ddc4b8f79addc1
krs131099 <krs.nandhan999@gmail.com>
2023-09-19 01:25:45 -0500

    Fixed unreflected changes to pointer argument.
    passed pointer to pointer to update pointer.
    
    Modified: 	tpool_pq.h
    		tpool_pq.c

--- tpool_pq.c
+++ tpool_pq.c
@@ -1,50 +1,134 @@
 #include "tpool_pq.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int init_tpool_pq() {
-    tpool_pq_t tpool_queue;
+
+/* TODO: add logging. */
+/* TODO: add fn to print task queue.*/
+
+
+void swap(task_t *tasks, int i, int j) {
+    task_t temp = *(tasks+i);
+    *(tasks+i) = *(tasks+j);
+    *(tasks+j) = temp;
 }
 
-// TODO: implement heap property.
+/* functions to maintain heap property*/
+void heapify_down(task_t *tasks, int heap_size, int i ) {
+    int left = 2* i;
+    int right = left + 1;
+    int largest = i;
+    //int left_priority = (tasks+left)->priority;
 
-// void swap(tpool_pq_t *tpool_queue, )
+    /* compare priorities of childs and swap*/
+    if (left <= heap_size && ((tasks+left)->priority > (tasks+i)->priority))
+        largest = left;
 
-int insert_task(tpool_pq_t *tpool_queue, task_t task) {
+    if (right <= heap_size && ((tasks+right)->priority > (tasks+largest)->priority)) 
+        largest = right;
     
-    pthread_mutex_lock(&(tpool_queue->mutex));
+    if (largest != i){
+        swap(tasks, i, largest);
+        heapify_down(tasks, heap_size, largest);
+    }
+}
+
+void heapify_up(task_t *tasks, int heap_size, int i) {
+    int parent = (int) i/2;
+    int largest = parent;
+
+    if (parent <= heap_size && (tasks+i)->priority > (tasks+parent)->priority) 
+        largest = i;
     
-    if (tpool_queue->n_tasks < MAX_QUEUE_SIZE) {
+    if (largest != parent) {
+        swap(tasks, i, largest);
+        /* heapify up if largest is not root*/
+        if (largest > 0)
+            heapify_up(tasks, heap_size, largest);
+    }
+}
+
+void build_heap(task_t *tasks, int n_tasks){
+    int start = (int) (n_tasks)/2;
+
+    for (int i = start; i >= 0; i--) {
+        heapify_down(tasks, n_tasks, i);
+    }
+}
+
+int init_tpool_pq(tpool_pq_t **tpool_pqs, int queue_size) {
+
+    //tpool_pq_t tpool_queue;
+    tpool_pq_t *tpool_pq;
+    if ((tpool_pq = (tpool_pq_t *) malloc(sizeof(struct tpool_pq))) == NULL)
+        printf("malloc returned null\n");
+    
+    // TODO: change static initialization to dynamic initialization;
+    tpool_pq->n_tasks = 0;
+    tpool_pq->queue_size = queue_size;
+    tpool_pq->task_array = (task_t *)malloc(sizeof(task_t)*queue_size);
+
+    /* call build heap only if some tasks available. TODO:  */
+    if (tpool_pq->n_tasks > 0)
+        build_heap(tpool_pq->task_array, tpool_pq->n_tasks);
+        
+    *tpool_pqs = tpool_pq;
+}
+
+
+
+int insert_task(tpool_pq_t **tpool_queue, task_t task) {
+    
+    // pthread_mutex_lock(&(tpool_queue->mutex));
+    // printf("Pool_queue: tasks: %d, queue_size = %d", tpool_queue->n_tasks, tpool_queue->queue_size);
+    
+    if ((*tpool_queue)->n_tasks < (*tpool_queue)->queue_size) {
         // insert task into queue
-        tpool_queue->task_array[tpool_queue->n_tasks++] = task;
+        int index = (*tpool_queue)->n_tasks;
+        (*tpool_queue)->task_array[(*tpool_queue)->n_tasks++] = task;
         // TODO: restore heap property.
+        int last_inserted = (*tpool_queue)->n_tasks;
+        heapify_up((*tpool_queue)->task_array, 0, last_inserted);
     }
 
     // release lock
-    pthread_mutex_unlock(&(tpool_queue->mutex));
+    // pthread_mutex_unlock(&(tpool_queue->mutex));
     return 1;
 }
 
-task_t *get_task(tpool_pq_t *tpool_queue) {
+task_t get_task(tpool_pq_t **tpool_queue) {
     
     task_t task;
-    pthread_mutex_lock(&(tpool_queue->mutex));
-    if (tpool_queue->n_tasks < MAX_QUEUE_SIZE) {
-        // insert task into queue
-        task = tpool_queue->task_array[0];
-        // decrease counter
-        tpool_queue->n_tasks--;
+    // pthread_mutex_lock(&(tpool_queue->mutex));
 
-        /*TODO: restore heap*/
+    if ((*tpool_queue)->n_tasks < MAX_QUEUE_SIZE) {
+        // insert task into queue
+        task = (*tpool_queue)->task_array[0];
+
+        // swap root task with last task.
+        swap((*tpool_queue)->task_array, 0, ((*tpool_queue)->n_tasks)-1);
+        // decrease counter
+        (*tpool_queue)->n_tasks--;
+
+        /*TODO: restore heap property.*/
+        heapify_down((*tpool_queue)->task_array, (*tpool_queue)->n_tasks, 0);
     }
     
-    pthread_mutex_unlock(&(tpool_queue->mutex));
-
-    return &task;
-    
+    // pthread_mutex_unlock(&(tpool_queue->mutex));
+    return task;    
 }
 
-int get_num_tasks(tpool_pq_t *tpool_queue) {
-    pthread_mutex_lock(&tpool_queue->mutex);
+int get_num_tasks(const tpool_pq_t *tpool_queue) {
+    //pthread_mutex_lock(&tpool_queue->mutex);
     int n = tpool_queue->n_tasks;
-    pthread_mutex_unlock(&tpool_queue->mutex);
+    //pthread_mutex_unlock(&tpool_queue->mutex);
     return n;
+}
+
+void print_tasks(tpool_pq_t *tpool_pq) {
+    int n = tpool_pq->n_tasks;
+
+    for (int i = 0; i < n; i++) {
+        printf("Task: %d, priority: %d\n", i, tpool_pq->task_array[i].priority);
+    }
 }
--- tpool_pq.h
+++ tpool_pq.h
@@ -3,28 +3,33 @@
 #define MAX_QUEUE_SIZE 64
 #define CONSTANT 10
 
-typedef enum priority {LOW, MEDIUM, HIGH} priority_t;
+typedef enum priority_t {LOW = 1, MEDIUM = 3, HIGH = 5} priority_t;
 
 typedef struct task_t {
     void (*function)();
     void *arguments;
-    //struct task_t *next;
+    /* struct task_t *next; */
     priority_t priority;
 } task_t;
 
 /* TODO: remove static declarations */
 typedef struct tpool_pq {
-    task_t task_array[MAX_QUEUE_SIZE];
-    int n_tasks = 0;
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    task_t *task_array; /* TODO: change to pointer. */
+    int n_tasks;
+    int queue_size;
+    /* pthread_mutex_t mutex; */
 } tpool_pq_t;
 
 
 // functions for priority queue
-int init_tpool_pq(tpool_pq_t *pq);
+int init_tpool_pq(tpool_pq_t **pq, int queue_size);
 
-int insert_task(tpool_pq_t *pq,task_t task);
+//int init_tpool_pq_list(tpool_pq_t *pq, int queue_size, tast_t *task_list);
 
-task_t *get_task(tpool_pq_t *tpool);
+int insert_task(tpool_pq_t **pq,task_t task);
+
+task_t get_task(tpool_pq_t **tpool);
 
 int get_num_tasks(const tpool_pq_t *tpool);
+
+void print_tasks(tpool_pq_t *tpool_pq);
